e018bf3dcef7465b4f296de7cbb52d3ec4b9ceee
shraddha319 <shraddha.jain3219@gmail.com>
2021-04-21 17:14:33 +0530

    feat: add toast component

---
+++ components/toast.js
@@ -0,0 +1,20 @@
+const toast = document.querySelector(".toast");
+const closeToastBtn = document.querySelector(".toast__close");
+const tirggerToast = document.getElementById("trigger--toast");
+
+const showToast = () => {
+    toast.classList.remove("toast--hide");
+    toast.classList.add("toast--show");
+}
+
+const hideToast = () => {
+    toast.classList.add("toast--hide");
+    toast.classList.remove("toast--show");
+};
+
+tirggerToast.addEventListener("click", () => {
+    showToast();
+    setTimeout(hideToast, 5000);
+});
+
+closeToastBtn.addEventListener("click", hideToast);
