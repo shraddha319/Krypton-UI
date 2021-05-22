ebeb80f371cdc4770f784dc2ebf5aa1eebc196e3
shraddha319 <shraddha.jain3219@gmail.com>
2021-05-22 13:26:27 +0530

    chore: removed unused files

--- components/navbar.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.getElementById("header__toggle");
-const nav = document.querySelector(".nav--main");
-
-toggleBtn.addEventListener("click", () => {
-    nav.style.display = (nav.style.display === "block") ?
-        "none" : "block";
-})
